7c4832488826378c054132412b827aa2694eb071
CodyNeeraj <Neerajkumar332424gmail.com>
2023-01-08 13:04:07 +0530

    Implemented Proptype configurations for type-checking and default beahviours

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Navbar() {
+
+function Navbar(props) {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">{props.title}</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="/">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="/">{props.about}</a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
@@ -38,5 +40,16 @@ function Navbar() {
     </div>
   )
 }
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired
+};
+
+Navbar.defaultProps = {
+  title: "TextUtils",
+  about: "About Us"
+};
+
 
 export default Navbar
